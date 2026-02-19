9b041477c9e6cf34274e5d1356741536cc35cab4
Prashantkhamitkar <prashantkhamitkar76@gmail.com>
2026-02-20 01:27:44 +0530

    Topic 2 data added

---
+++ Topic_2/Code/testapp/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('This is My First Angular Application');
+  protected readonly message=signal('Build Using TypeScript');
+}
